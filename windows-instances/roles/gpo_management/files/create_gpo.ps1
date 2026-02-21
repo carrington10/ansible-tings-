2a96a8868a3cd61d08fc3c349aede4de818c8dd2
nonne <carringtonhate@gmail.com>
2026-02-21 17:49:13 -0600

    adding in gpo role

--- windows-instances/roles/gpo_management/files/create_gpo.ps1
+++ windows-instances/roles/gpo_management/files/create_gpo.ps1
@@ -7,22 +7,37 @@ param (
 
 )
 Import-Module grouppolicy
-$gpo = get-gpo -name $gponame
+$changed = $false;
+$gpo = get-gpo -name $gponame -erroraction silentlycontinue
 
     if(-not $gpo)
     {
         write-host " creating gpo  $gponame"
-        new-gpo -name $gponame
+        new-gpo -name $gponame | out-null
+        $changed = $true;
     }
     else{
         write-host "gpo already existts."
     }
 # link gpo if not already linked 
-$inheritance = get-inheritance -target $targetOu
-    if ($inheritance.gpolinks.displayname -notcontains $gponame){
-    write-host " linking gpo to ou "
-    new-gplink -name $gponame -target $targetou -enforced yes
+$inheritance =  Get-GPInheritance -target $targetOu
+$linked = $false;
+    if ($inheritance.GpoLinks) {
+        foreach ($link in $inheritance.GpoLinks) {
+            if ($link.DisplayName -eq $gponame) {
+                $linked = $true;
+                write-host "gpo already linked to ou."
+            }
+        }
     }
-    else {
-        write-host " gpo already linked."
+    if (-not $linked){
+        write-host "linking gpo to ou $targetOu"
+         new-grolink -name $gponame -target $targetOu | out-null
+         $changed = $true;
     }
+    else{
+        write-host "gpo already linked to ou."
+    }
+[PSCustomObject]@{
+    changed = $changed
+}] | convert-json -compress
