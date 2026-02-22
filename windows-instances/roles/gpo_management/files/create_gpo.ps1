782207199a03704f5a827fe30b2d58e519dbab49
nonne <carringtonhate@gmail.com>
2026-02-21 18:34:11 -0600

    adding in gpo role

--- windows-instances/roles/gpo_management/files/create_gpo.ps1
+++ windows-instances/roles/gpo_management/files/create_gpo.ps1
@@ -1,11 +1,42 @@
+# powershell script to create a gpo and linked it to a given ou
+
 param (
-    [string]$gponame,
-    [string]$targetou
+    [string] $gponame,
+    [string] $targetou
+
 )
+Import-Module grouppolicy
+$changed = $false;
+$gpo = get-gpo -name $gponame -erroraction silentlycontinue
 
-$ErrorActionPreference = "Stop"
-
+    if(-not $gpo)
+    {
+        write-host " creating gpog $gponame"
+        new-gpo -name $gponame | out-null
+        $changed = $true;
+    }
+    else{
+        write-host "gpo already existts."
+    }
+# link gpo if not already linked 
+$inheritance =  Get-GPInheritance -target $targetou
+$linked = $false;
+    if ($inheritance.GpoLinks) {
+        foreach ($link in $inheritance.GpoLinks) {
+            if ($link.DisplayName -eq $gponame) {
+                $linked = $true;
+                write-host "gpo already linked to ou."
+            }
+        }
+    }
+    if (-not $linked){
+        write-host "linking gpo to ou $targetuu"
+         new-gplink -name $gponame -target $targetou | out-null
+         $changed = $true;
+    }
+    else{
+        write-host "gpo already linked to ou."
+    }
 [PSCustomObject]@{
-    received_gpo = $gponame
-    received_ou  = $targetou
-} | ConvertTo-Json -Compress
+    changed = $changed
+} | convertto-json -compress
