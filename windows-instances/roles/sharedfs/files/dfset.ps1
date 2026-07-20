20fa920ab1adc4f086bc248197c718c9020e69f4
nonne <carringtonhate@gmail.com>
2026-07-20 01:29:29 -0500

    adding in dfs script

--- windows-instances/roles/sharedfs/files/dfset.ps1
+++ windows-instances/roles/sharedfs/files/dfset.ps1
@@ -1,8 +1,26 @@
+
 param(
     [string] $computer,
-    [string] $dfsfolder,
+    [string] $dfsfolder
 )
-## create dfs set namespace
-new-dfsnroot -path "\\kingdom.local\shares" -targetpath "\\$computer\shares" -type domainv2 -erroraction silentlycontinue
-## create dfs folder 
-new-dfsnfolder -path "\\kingdom.local\shares\$dfsfolder" -targetpath "\\$computer\$dfsfolder" -erroraction silentlycontinue
+
+$rootPath = "\\kingdom.local\shares"
+$folderPath = "\\kingdom.local\shares\$dfsfolder"
+$changedRoot = $false
+$changedFolder = $false
+if (-not (Get-DfsnRoot -Path $rootPath -ErrorAction SilentlyContinue)) {
+    New-DfsnRoot -Path $rootPath -TargetPath "\\$computer\shares" -Type DomainV2
+    $changedRoot = $true
+}
+
+if (-not (Get-DfsnFolder -Path $folderPath -ErrorAction SilentlyContinue)) {
+    New-DfsnFolder -Path $folderPath -TargetPath "\\$computer\$dfsfolder"
+    $changedFolder = $true
+}
+
+$result = @{
+    changedRoot = $changedRoot
+    changedFolder = $changedFolder
+}
+
+write-output ($result | ConvertTo-Json -Compress)
